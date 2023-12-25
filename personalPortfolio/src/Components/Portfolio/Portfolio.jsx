2a5cdf89d9a1b9874ae1bf80a48540a88cef5928
unknown <pawanb78@gmail.com>
2023-12-25 14:44:21 +0000

    project added

--- personalPortfolio/src/Components/Portfolio/Portfolio.jsx
+++ personalPortfolio/src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import './Portfolio.css'
+import Portfolio1 from '../../assets/portfolio1.png'
+import Portfolio2 from '../../assets/portfolio2.png'
+import Portfolio3 from '../../assets/portfolio3.png'
 
 
 const Portfolio = () => {
+
+  
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className='portfolio_item'>
+            <div className="portfolio_item-image">
+              <img src={Portfolio1} alt="first-project"className='portfolio_item-img'/>
+            </div>
+              <h3>This is a portfolio item Title</h3>
+              <div className="portfolio_item-cte">
+                <a href="https://github.com" className='btn btn-primary'>Project 1</a>
+                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+              </div>
+        </article>
+        <article className='portfolio_item'>
+            <div className="portfolio_item-image">
+              <img src={Portfolio1} alt="first-project" className='portfolio_item-img'/>
+            </div>
+              <h3>This is a portfolio item Title</h3>
+              <div className="portfolio_item-cte">
+                <a href="https://github.com" className='btn btn-primary'>Project 1</a>
+                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+              </div>
+        </article>
+        <article className='portfolio_item'>
+            <div className="portfolio_item-image">
+              <img src={Portfolio1} alt="first-project" className='portfolio_item-img'/>
+            </div>
+              <h3>This is a portfolio item Title</h3>
+              <div className="portfolio_item-cte">
+                <a href="https://github.com" className='btn btn-primary'>Project 1</a>
+                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+              </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
