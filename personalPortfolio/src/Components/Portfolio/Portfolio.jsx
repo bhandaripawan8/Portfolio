15a2953563b04347e3102dfbf47d6684a9b4c213
unknown <pawanb78@gmail.com>
2023-12-26 01:04:27 +0000

    files added

--- personalPortfolio/src/Components/Portfolio/Portfolio.jsx
+++ personalPortfolio/src/Components/Portfolio/Portfolio.jsx
@@ -7,6 +7,30 @@ import Portfolio3 from '../../assets/portfolio3.png'
 
 const Portfolio = () => {
 
+  const portfolio = [
+    {
+      id: 1,
+      Title: "NCC-Airline",
+      image: Portfolio1,
+      github: 'https://github.com/bhandaripawan8/NCC-Airlines/tree/main/OneDrive%20-%20University%20of%20Gloucestershire/Desktop/react%20ss%20notes/ncc%20airlines',
+      demo: 'https://github.com'
+    },
+    {
+      id: 2,
+      Title: "Forest",
+      image: Portfolio3,
+      github: 'https://github.com/bhandaripawan8/forestassignment',
+      demo: 'https://github.com'
+    },
+    {
+      id: 3,
+      Title: "Tindog",
+      image: Portfolio2,
+      github: 'https://github.com/bhandaripawan8/tindog',
+      demo: 'https://github.com'
+    }
+  ]
+
   
   return (
     <section id='portfolio'>
@@ -14,36 +38,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
-        <article className='portfolio_item'>
-            <div className="portfolio_item-image">
-              <img src={Portfolio1} alt="first-project"className='portfolio_item-img'/>
-            </div>
-              <h3>This is a portfolio item Title</h3>
-              <div className="portfolio_item-cte">
-                <a href="https://github.com" className='btn btn-primary'>Project 1</a>
-                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
-              </div>
-        </article>
-        <article className='portfolio_item'>
-            <div className="portfolio_item-image">
-              <img src={Portfolio1} alt="first-project" className='portfolio_item-img'/>
-            </div>
-              <h3>This is a portfolio item Title</h3>
-              <div className="portfolio_item-cte">
-                <a href="https://github.com" className='btn btn-primary'>Project 1</a>
-                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
-              </div>
-        </article>
-        <article className='portfolio_item'>
-            <div className="portfolio_item-image">
-              <img src={Portfolio1} alt="first-project" className='portfolio_item-img'/>
-            </div>
-              <h3>This is a portfolio item Title</h3>
-              <div className="portfolio_item-cte">
-                <a href="https://github.com" className='btn btn-primary'>Project 1</a>
-                <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
-              </div>
-        </article>
+          {
+            portfolio.map(({id, image, Title, github, demo})=>{
+              return(
+                <article key = {id} className='portfolio_item'>
+                <h3>{Title}</h3>
+                <div className="portfolio_item-image">
+                  <img src={image} alt="first-project"className='portfolio_item-img'/>
+                </div>
+                  <div className="portfolio_item-cte">
+                    <a href={github} className='btn btn-primary'>Github</a>
+                    <a href={demo} className='btn btn-primary'>Demo</a>
+                  </div>
+                </article>
+              )
+            })
+          }
       </div>
     </section>
   )
--- personalPortfolio/src/Components/Testimonials/Testimonials.jsx
+++ personalPortfolio/src/Components/Testimonials/Testimonials.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
 import './Testimonials.css'
+import testimonials1 from '../../assets/testimonials1.png'
+import testimonials2 from '../../assets/testimonials1.png'
+import testimonials3 from '../../assets/testimonials1.png'
+import testimonials4 from '../../assets/testimonials1.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/pagination'; 
+import 'swiper/css/navigation'; // Import Swiper navigation styles
+import 'swiper/css'; // Import Swiper styles
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 const Testimonials = () => {
+
+  const data = [
+    {
+      id: 1,
+      avatar: testimonials1,
+      name: 'Jeremy',
+      review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum dolor eveniet necessitatibus, quisquam dolores repellat esse? A eum aut error, maiores eveniet quis, voluptates harum consectetur aperiam quod, ad ex ipsa. Rerum sint tempore voluptatum eaque non dolores debitis accusantium corrupti possimus facilis? Ullam nisi libero molestiae doloremque a! Minima.'
+    },
+    {
+      id: 2,
+      avatar: testimonials2,
+      name: 'Andy Kok',
+      review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum dolor eveniet necessitatibus, quisquam dolores repellat esse? A eum aut error, maiores eveniet quis, voluptates harum consectetur aperiam quod, ad ex ipsa. Rerum sint tempore voluptatum eaque non dolores debitis accusantium corrupti possimus facilis? Ullam nisi libero molestiae doloremque a! Minima.'
+    },
+    {
+      id: 3,
+      avatar: testimonials3,
+      name: 'Sri',
+      review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum dolor eveniet necessitatibus, quisquam dolores repellat esse? A eum aut error, maiores eveniet quis, voluptates harum consectetur aperiam quod, ad ex ipsa. Rerum sint tempore voluptatum eaque non dolores debitis accusantium corrupti possimus facilis? Ullam nisi libero molestiae doloremque a! Minima.'
+    },
+    {
+      id: 4,
+      avatar: testimonials4,
+      name: 'Rajendra',
+      review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum dolor eveniet necessitatibus, quisquam dolores repellat esse? A eum aut error, maiores eveniet quis, voluptates harum consectetur aperiam quod, ad ex ipsa. Rerum sint tempore voluptatum eaque non dolores debitis accusantium corrupti possimus facilis? Ullam nisi libero molestiae doloremque a! Minima.'
+    },
+  ]
+
+
   return (
-    <div>Testimonials</div>
+    <section id='testimonials'>
+        <h5>Review from clients</h5>
+        <h2>Testimonials</h2>
+
+        <div className="container testimonials_container">
+
+          <Swiper 
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                direction="horizontal" // Set the direction to horizontal
+                spaceBetween={40} // Adjust the space between slides
+                slidesPerView={1} // Adjust the number of slides visible at once
+                pagination={{ clickable: true }}
+                // Navigation
+                // className="swiper-wrapper"
+                 scrollbar={{draggable:true}}
+              >
+              {
+              data.map(({avatar,name,review, id})=>{
+                return(
+                <SwiperSlide key={id} className='testimonials'>
+                  <div className="client_avatar">
+                    <img src={avatar} alt="firstTestimonials" />
+                  </div>
+                  <h5 className='client_name'>{name}</h5>
+                    <small className='client_review'>{review}</small>
+                </SwiperSlide>
+                )
+              })
+            }
+            </Swiper>
+        </div>
+    </section>
   )
 }
 
